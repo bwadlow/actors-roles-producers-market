4555e1ddd2ca30472103f88b3cfb2fb6dbcbf38f
bwadlow <bwadlow@gmail.com>
2018-08-20 17:36:07 -0400

    added index and show pages

--- app/controllers/production_companies_controller.rb
+++ app/controllers/production_companies_controller.rb
@@ -1,2 +1,28 @@
 class ProductionCompaniesController < ApplicationController
-end
+
+  def index
+    @production_companies = ProductionCompany.all
+  end
+
+  def show
+    @production_company = ProductionCompany.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+  end
+
+
+  private
+
+  def prod_params
+    params.require(:production_companies).permit(:name)
+  end
+
+
+
+
+end# end of class
