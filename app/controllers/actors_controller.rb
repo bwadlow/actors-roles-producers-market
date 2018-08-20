2406faef03eb4c37bdc4a9cf731d0a1546ec5e2f
github username <github email address>
2018-08-20 17:35:17 -0400

    actors index new and show pages done

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,31 @@
 class ActorsController < ApplicationController
+
+  def index
+    @actors = Actor.all
+  end
+
+  def show
+    @actor = Actor.find(params[:id])
+  end
+
+  def new
+    @sex_array = ['male', 'female', 'other']
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.create(actor_params)
+    if @actor
+      redirect_to actor_path(@actor)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def actor_params
+    params.require(:actor).permit(:name, :age, :sex)
+  end
+
 end
--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,4 +2,5 @@ class Actor < ApplicationRecord
   has_many :auditions
   has_many :bookings
   has_many :production_companies, through: :bookings
+
 end
