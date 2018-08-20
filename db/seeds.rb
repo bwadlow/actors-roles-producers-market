d2ebe3263e2c68844f4408a73a08bdf47184df51
github username <github email address>
2018-08-20 15:48:11 -0400

    seeds tentatively working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+sex_array = ['male', 'female', 'other']
+
+20.times do
+  Actor.create(name: Faker::Name.name, age: rand(18..99), sex: sex_array.sample(1).first)
+end
+
+20.times do
+  ProductionCompany.create(name: Faker::Company.name)
+end
+
+50.times do
+  Booking.create!(salary: rand(100..5000), duration_days: rand(1..365), show_name: Faker::Book.title, role_name: Faker::Name.first_name, available?: [true, false].sample, actor_id: rand(1..20))
+end
