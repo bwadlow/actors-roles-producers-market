f9812daa249ce3846f65532b241219fdddf73e6c
github username <github email address>
2018-08-21 10:24:18 -0400

    added date change to auditions

--- app/controllers/auditions_controller.rb
+++ app/controllers/auditions_controller.rb
@@ -8,9 +8,28 @@ class AuditionsController < ApplicationController
     @audition = Audition.find(params[:id])
   end
 
+  def edit
+    @audition = Audition.find(params[:id])
+  end
+
+  def update
+    @audition = Audition.update(audition_params)
+    if @audition
+      redirect_to audition_path(@audition)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @audition = Audition.destroy(params[:id])
     redirect_to auditions_path
+  end
+
+  private
+
+  def audition_params
+    params.require(:audition).permit(:address, :time, :actor_id, :production_company_id, :booking_id)
   end
 
 end
