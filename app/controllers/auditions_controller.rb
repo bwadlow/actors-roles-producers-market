a5ae875664daedfb35f83c939816430f0f093c4b
github username <github email address>
2018-08-21 09:44:23 -0400

    delete auditions added

--- app/controllers/auditions_controller.rb
+++ app/controllers/auditions_controller.rb
@@ -1,2 +1,16 @@
 class AuditionsController < ApplicationController
+
+  def index
+    @auditions = Audition.all
+  end
+
+  def show
+    @audition = Audition.find(params[:id])
+  end
+
+  def destroy
+    @audition = Audition.destroy(params[:id])
+    redirect_to auditions_path
+  end
+
 end
