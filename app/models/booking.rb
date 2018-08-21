91bbef8fb8b88590e9e3d2fef4f5b4ed2ec5f883
github username <github email address>
2018-08-21 18:01:41 -0400

    search function started

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,8 @@ class Booking < ApplicationRecord
   has_many :auditions
   belongs_to :actor, optional: true
   belongs_to :production_company
+
+  scope :sex_input, -> { where(sex: 'sex_input') }
+
+  scope :teens, -> { where(:age) }
 end
