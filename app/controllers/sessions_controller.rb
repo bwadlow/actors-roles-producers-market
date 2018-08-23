bcc3815b5dd329cf99f3fcc88fb8d5d525b73b99
github username <github email address>
2018-08-23 14:37:14 -0400

    login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,15 @@ def new
 end
 
 def create
+  # byebug
   @actor = Actor.find_by ({username: params[:username]})
   @production_company = ProductionCompany.find_by ({username: params[:username]})
-  if !!@actor && @actor.authentiate(params[:password])
+  if !!@actor && @actor.authenticate(params[:password])
     flash[:notice] = "Success"
     session[:actor_id] = @actor.id #creates hash in session
     redirect_to @actor
   elsif
-    !!@production_company && @production_company.authentiate(params[:password])
+    !!@production_company && @production_company.authenticate(params[:password])
     flash[:notice] = "Success"
     session[:production_company_id] = @production_company.id
     redirect_to @production_company
