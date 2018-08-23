d6f70e3013c998df09802c76ae319206c461b832
github username <github email address>
2018-08-23 13:10:37 -0400

    sessions controller destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,17 @@ def new
 end
 
 def create
-  @user = User.find_by ({username: params[:username]})
-  if !!@user && @user.authentiate(params[:password])
+  @actor = Actor.find_by ({username: params[:username]})
+  @production_company = ProductionCompany.find_by ({username: params[:username]})
+  if !!@actor && @actor.authentiate(params[:password])
     flash[:notice] = "Success"
-    session[:user_id] = @user.id #creates hash in session
-    redirect_to @user
+    session[:actor_id] = @actor.id #creates hash in session
+    redirect_to @actor
+  elsif
+    !!@production_company && @production_company.authentiate(params[:password])
+    flash[:notice] = "Success"
+    session[:production_company_id] = @production_company.id
+    redirect_to @production_company
   else
     flash[:notice] = "Invalid login information"
     redirect_to login_path
