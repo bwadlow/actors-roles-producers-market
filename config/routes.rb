e66339481e5deda80e0620affc3f96d84427832c
github username <github email address>
2018-08-23 16:56:08 -0400

    small change

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ Rails.application.routes.draw do
   resources :production_companies
   resources :actors
   resources :users, except: :show
-  get '/actorprofile', to: 'actors#profile', as: 'actorprofile'
-  get '/actorprofile/edit', to: 'actors#edit', as: 'edit_actorprofile'
-  get '/productioncompanyprofile', to: 'production_companies#profile', as: 'productioncompanyprofile'
-  get '/productioncompanyprofile/edit', to: 'production_companies#edit', as: 'edit_production_companyprofile'
+  
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
